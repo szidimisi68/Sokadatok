149077047be85c54af5a56f9dd247456bca390e3
szidimisi68 <70961855+szidimisi68@users.noreply.github.com>
2023-11-08 13:08:43 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using ConsoleAppOrgBig;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+            List<Organization> organizations = new List<Organization>();
+        public MainWindow()
+        {
+            File.ReadAllLines("C:\\teszt\\organizations-100000.csv").Skip(1).ToList().ForEach(x => organizations.Add(new Organization(x.Split(";"))));
+            
+            InitializeComponent();
+
+            List<string> orszagok = new List<string>();
+            orszagok.Add("none");
+            organizations.Select(x => x.Country).Distinct().OrderBy(x => x).ToList().ForEach(x => orszagok.Add(x));
+            cbOrszag.ItemsSource = orszagok;
+
+            List<string> evek = new List<string>();
+            evek.Add("none");
+            organizations.Select(x => x.Founded).Distinct().OrderBy(x => x).ToList().ForEach(x => evek.Add(Convert.ToString(x)));
+            cbEv.ItemsSource = evek;
+
+            dgAdatok.ItemsSource = organizations;
+            lbOsszes.Content = $"Összes: {organizations.Sum(x => x.EmployeesNumber)}";
+        }
+
+        private void cbOrszag_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<Organization> szurt = organizations;
+
+            string orszag = cbOrszag.SelectedItem.ToString();
+            if (cbOrszag.SelectedIndex != 0 && cbOrszag.SelectedIndex != -1)
+            {
+                szurt = orszagSzures(szurt, orszag);
+            }
+            int ev = Convert.ToInt32(cbEv.SelectedItem);
+
+            if (cbEv.SelectedIndex != 0 && cbEv.SelectedIndex != -1)
+            {
+                szurt = evSzures(szurt, ev);
+            }
+            dgAdatok.ItemsSource = szurt;
+            lbOsszes.Content = $"Összes: {szurt.Sum(x => x.EmployeesNumber)}";
+
+        }
+        private void cbEv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<Organization> szurt = organizations;
+
+            string orszag = cbOrszag.SelectedItem.ToString();
+            if (cbOrszag.SelectedIndex != 0 && cbOrszag.SelectedIndex != -1)
+            {
+                szurt = orszagSzures(szurt, orszag);
+            }
+            int ev = Convert.ToInt32(cbEv.SelectedItem);
+
+            if (cbEv.SelectedIndex != 0 && cbEv.SelectedIndex != -1)
+            {
+                szurt = evSzures(szurt, ev);
+            }
+            dgAdatok.ItemsSource = szurt;
+            lbOsszes.Content = $"Összes: {szurt.Sum(x => x.EmployeesNumber)}";
+        }
+
+        static List<Organization> orszagSzures(List<Organization> szurt, string orszag)
+        {
+            szurt = szurt.Where(x => x.Country == orszag).ToList();
+            return szurt;
+        }
+
+        static List<Organization> evSzures(List<Organization> szurt, int ev)
+        {
+            szurt = szurt.Where(x => x.Founded == ev).ToList();
+            return szurt;
+        }
+
+    }
+}
---
+++ Organization.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOrgBig
+{
+    public class Organization
+    {
+        int index;
+        string id;
+        string name;
+        string website;
+        string country;
+        string description;
+        int founded;
+        string industry;
+        int employeesNumber;
+
+        public Organization(string[] mezok)
+        {
+            if (mezok.Length != 9)
+                throw new ArgumentException("Nem megfelelő számú mező!");
+
+            if (!int.TryParse(mezok[0], out this.index))
+                throw new ArgumentException("Hibás értékű mező! [index]");
+            this.id = mezok[1];
+            this.name = mezok[2];
+            this.website = mezok[3];
+            this.country = mezok[4];
+            this.description = mezok[5];
+            if (!int.TryParse(mezok[6], out this.founded))
+                throw new ArgumentException("Hibás értékű mező! [founded]");
+            this.industry = mezok[7];
+            if (!int.TryParse(mezok[8], out this.employeesNumber))
+                throw new ArgumentException("Hibás értékű mező! [employeesNumber]");
+        }
+
+        public int Index { get => index; }
+        public string Id { get => id; }
+        public string Name { get => name; }
+        public string Website { get => website; }
+        public string Country { get => country; }
+        public string Description { get => description; }
+        public int Founded { get => founded; }
+        public string Industry { get => industry; }
+        public int EmployeesNumber { get => employeesNumber; }
+    }
+}
